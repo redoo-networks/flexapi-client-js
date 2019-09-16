6a6361a384dd53c3f2c6dfae28870ae1219a7b26
Mikhailo Hornostai <mivigo95@gmail.com>
2019-09-16 17:02:13 +0300

    cleanup

--- components/record.js
+++ components/record.js
@@ -27,12 +27,6 @@ export default class Record {
         return FlexAPI.post('records/' + this.moduleName + '/' + this.recordId, parameters).then((response) => {
             return response;
         });
-
-        // return new Promise((resolve) => {
-        //     FlexAPI.post('records/' + this.moduleName + '/' + this.recordId, parameters).then((response) => {
-        //         resolve(response);
-        //     });
-        // });
     }
 
 
