efe44a1666a25caee8ac5e92de343e537df2cf01
Stefan Warnat <warnat@redoo-networks.com>
2019-09-25 15:35:39 +0200

    Add Basic Auth Add CRMLogin Method

--- flexapi.js
+++ flexapi.js
@@ -13,6 +13,19 @@ class FlexAPI {
         this.token = token;
     }
 
+    setCRMLogin(username, password) {
+        this.request('login/login', { username: username, password:password}).then((response) => {
+            console.log(response);
+        })
+    }
+
+    setBasicAuth(username, password) {
+        this.basicAuth = {
+            username: username,
+            password: password
+        }
+    }
+
     module(moduleName) {
         return new Module(moduleName);
     }
@@ -36,7 +49,12 @@ class FlexAPI {
             }
             data.params = parameters;
 
-            axios.post(this.url, data)
+            let parameters = {};
+            if(typeof this.basicAuth !== 'undefined') {
+                parameters['auth'] = this.basicAuth;
+            }
+
+            axios.post(this.url, data, parameters)
                 .then((response) => {
                     resolve(response.data.data);
                 }, () => {
